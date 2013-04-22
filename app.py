45b934ce66f71c918c2746210b43ce6e8c4ef60a
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-04-22 16:09:52 -0400

    Updates app to be a thin shell over mongodb.

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+import datetime
+import json
+
 from flask import Flask, render_template
 from pymongo import MongoClient, DESCENDING, ASCENDING
 
@@ -11,20 +14,26 @@ db = client.apps
 menu_items = db.menu_items
 
 
-@app.route('/week/<week_number>/')
-def list_by_week(week_number):
-    context = {"menu_items": []}
-    for item in menu_items.find({"week": int(week_number)}):
-        context['menu_items'].append(item)
-    return render_template('index.html', **context)
+@app.route('/raw/')
+def raw_find():
+    from flask import request
+    q = request.args.get('q', None)
 
+    query = menu_items.find()
+    if q:
+        query = menu_items.find(json.loads(q))
 
-@app.route('/')
-def list_all():
-    context = {"menu_items": []}
-    for item in menu_items.find():
-        context['menu_items'].append(item)
-    return render_template('index.html', **context)
+    response = {}
+    response['items'] = []
+    start = datetime.datetime.now()
+    for item in query:
+        del(item['_id'])
+        response['items'].append(item)
+    response['meta'] = {}
+    response['meta']['count'] = len(response['items'])
+    elapsed = datetime.datetime.now() - start
+    response['meta']['time_elapsed'] = float('%s.%s' % (elapsed.seconds, elapsed.microseconds))
+    return json.dumps(response)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
